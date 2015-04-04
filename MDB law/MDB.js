e4bb20ec38b46c65773af22f0da05dfe52cf4fed
Greeng0 <eminalex@hotmail.com>
2015-04-04 15:44:42 -0400

    More tweaks
    - Adjusted image backgrounds
    - Prepped translations
    - Simplified translations bindings in page

--- MDB law/MDB.js
+++ MDB law/MDB.js
@@ -1,5 +1,5 @@
 ﻿var mdbViewModel = (function mdbModel() {
-	var self = this;
+	var self = {};
 	
 	self.language = ko.observable("en");
 	self.tab = ko.observable("home");
@@ -11,7 +11,12 @@
 		labelNavContent: "INFORMATION",
 		labelNavContact: "CONTACT US",
 		labelRights: "All rights reserved",
-		labelDesignedBy: "Web Design:"
+		labelDesignedBy: "Web Design:",
+		labelHomeTitle: "Title of stuff",
+		labelHomeP1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas adipiscing at arcu ut vulputate. Cras enim tellus, scelerisque vitae accumsan ac, laoreet tempus enim. Nam adipiscing dictum augue, egestas feugiat nibh interdum et. Nam aliquam fermentum aliquet. Morbi id massa consequat, suscipit sem ut, scelerisque justo. Phasellus quis porta tortor. Praesent nec mauris laoreet, luctus diam sagittis, vehicula ante. Suspendisse ullamcorper varius purus vitae ultrices. Sed vel condimentum metus, vitae laoreet est. Proin at rutrum massa, in sodales sapien. Suspendisse ornare eros sagittis augue lobortis, vel venenatis sapien tincidunt. Praesent sed sodales eros. Interdum et malesuada fames ac ante ipsum primis in faucibus.",
+		labelHomeP2: "Donec eu arcu venenatis, egestas risus eget, pretium est. Donec commodo in nisl non iaculis. Vestibulum quis enim sit amet ante iaculis eleifend sed ut nisi. Sed luctus tellus urna. Vivamus semper lacinia mauris, eu cursus dui imperdiet vel. Morbi scelerisque, nisi feugiat euismod congue, eros metus vestibulum lorem, eget dignissim leo massa id lorem. Cras accumsan lectus eu eros ultricies aliquet. Nulla ante lacus, vehicula porta iaculis eget, pellentesque vel nisl. Etiam imperdiet interdum pretium. Cras ultricies eu diam in feugiat. Vivamus scelerisque viverra neque, nec bibendum lorem tempor id. Nulla et arcu ac purus pharetra dignissim. Sed cursus dui eu nisl consectetur imperdiet. In hendrerit facilisis lacus, et commodo magna fringilla sed. Integer facilisis dignissim leo, ac fringilla augue pretium ac.",
+		labelServices: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas adipiscing at arcu ut vulputate. Cras enim tellus, scelerisque vitae accumsan ac, laoreet tempus enim. Nam adipiscing dictum augue, egestas feugiat nibh interdum et. Nam aliquam fermentum aliquet. Morbi id massa consequat, suscipit sem ut, scelerisque justo. Phasellus quis porta tortor. Praesent nec mauris laoreet, luctus diam sagittis, vehicula ante. Suspendisse ullamcorper varius purus vitae ultrices. Sed vel condimentum metus, vitae laoreet est. Proin at rutrum massa, in sodales sapien. Suspendisse ornare eros sagittis augue lobortis, vel venenatis sapien tincidunt. Praesent sed sodales eros. Interdum et malesuada fames ac ante ipsum primis in faucibus.",
+		labelContent: "Donec eu arcu venenatis, egestas risus eget, pretium est. Donec commodo in nisl non iaculis. Vestibulum quis enim sit amet ante iaculis eleifend sed ut nisi. Sed luctus tellus urna. Vivamus semper lacinia mauris, eu cursus dui imperdiet vel. Morbi scelerisque, nisi feugiat euismod congue, eros metus vestibulum lorem, eget dignissim leo massa id lorem. Cras accumsan lectus eu eros ultricies aliquet. Nulla ante lacus, vehicula porta iaculis eget, pellentesque vel nisl. Etiam imperdiet interdum pretium. Cras ultricies eu diam in feugiat. Vivamus scelerisque viverra neque, nec bibendum lorem tempor id. Nulla et arcu ac purus pharetra dignissim. Sed cursus dui eu nisl consectetur imperdiet. In hendrerit facilisis lacus, et commodo magna fringilla sed. Integer facilisis dignissim leo, ac fringilla augue pretium ac."
 	};
 	self.frMap = {
 		labelLang: "ENGLISH",
@@ -20,7 +25,12 @@
 		labelNavContent: "INFORMATION",
 		labelNavContact: "NOUS REJOINDRE",
 		labelRights: "Tous droits réservés",
-		labelDesignedBy: "Réalisation:"
+		labelDesignedBy: "Réalisation:",
+		labelHomeTitle: "Titre de page",
+		labelHomeP1: "French ipsum dolor sit amet, consectetur adipiscing elit. Maecenas adipiscing at arcu ut vulputate. Cras enim tellus, scelerisque vitae accumsan ac, laoreet tempus enim. Nam adipiscing dictum augue, egestas feugiat nibh interdum et. Nam aliquam fermentum aliquet. Morbi id massa consequat, suscipit sem ut, scelerisque justo. Phasellus quis porta tortor. Praesent nec mauris laoreet, luctus diam sagittis, vehicula ante. Suspendisse ullamcorper varius purus vitae ultrices. Sed vel condimentum metus, vitae laoreet est. Proin at rutrum massa, in sodales sapien. Suspendisse ornare eros sagittis augue lobortis, vel venenatis sapien tincidunt. Praesent sed sodales eros. Interdum et malesuada fames ac ante ipsum primis in faucibus.",
+		labelHomeP2: "Francais eu arcu venenatis, egestas risus eget, pretium est. Donec commodo in nisl non iaculis. Vestibulum quis enim sit amet ante iaculis eleifend sed ut nisi. Sed luctus tellus urna. Vivamus semper lacinia mauris, eu cursus dui imperdiet vel. Morbi scelerisque, nisi feugiat euismod congue, eros metus vestibulum lorem, eget dignissim leo massa id lorem. Cras accumsan lectus eu eros ultricies aliquet. Nulla ante lacus, vehicula porta iaculis eget, pellentesque vel nisl. Etiam imperdiet interdum pretium. Cras ultricies eu diam in feugiat. Vivamus scelerisque viverra neque, nec bibendum lorem tempor id. Nulla et arcu ac purus pharetra dignissim. Sed cursus dui eu nisl consectetur imperdiet. In hendrerit facilisis lacus, et commodo magna fringilla sed. Integer facilisis dignissim leo, ac fringilla augue pretium ac.",
+		labelServices: "Quelque chose ipsum dolor sit amet, consectetur adipiscing elit. Maecenas adipiscing at arcu ut vulputate. Cras enim tellus, scelerisque vitae accumsan ac, laoreet tempus enim. Nam adipiscing dictum augue, egestas feugiat nibh interdum et. Nam aliquam fermentum aliquet. Morbi id massa consequat, suscipit sem ut, scelerisque justo. Phasellus quis porta tortor. Praesent nec mauris laoreet, luctus diam sagittis, vehicula ante. Suspendisse ullamcorper varius purus vitae ultrices. Sed vel condimentum metus, vitae laoreet est. Proin at rutrum massa, in sodales sapien. Suspendisse ornare eros sagittis augue lobortis, vel venenatis sapien tincidunt. Praesent sed sodales eros. Interdum et malesuada fames ac ante ipsum primis in faucibus.",
+		labelContent: "Affaires eu arcu venenatis, egestas risus eget, pretium est. Donec commodo in nisl non iaculis. Vestibulum quis enim sit amet ante iaculis eleifend sed ut nisi. Sed luctus tellus urna. Vivamus semper lacinia mauris, eu cursus dui imperdiet vel. Morbi scelerisque, nisi feugiat euismod congue, eros metus vestibulum lorem, eget dignissim leo massa id lorem. Cras accumsan lectus eu eros ultricies aliquet. Nulla ante lacus, vehicula porta iaculis eget, pellentesque vel nisl. Etiam imperdiet interdum pretium. Cras ultricies eu diam in feugiat. Vivamus scelerisque viverra neque, nec bibendum lorem tempor id. Nulla et arcu ac purus pharetra dignissim. Sed cursus dui eu nisl consectetur imperdiet. In hendrerit facilisis lacus, et commodo magna fringilla sed. Integer facilisis dignissim leo, ac fringilla augue pretium ac."
 	};
 	
 	self.getTranslation = function (key) {
@@ -33,13 +43,7 @@
 		return map[key];
 	};
 	self.currentYear = ko.computed(function () { return new Date().getFullYear(); });
-	self.labelLang = ko.computed(function () { return self.getTranslation("labelLang"); });
-	self.labelNavHome = ko.computed(function () { return self.getTranslation("labelNavHome"); });
-	self.labelNavServices = ko.computed(function () { return self.getTranslation("labelNavServices"); });
-	self.labelNavContent = ko.computed(function () { return self.getTranslation("labelNavContent"); });
-	self.labelNavContact = ko.computed(function () { return self.getTranslation("labelNavContact"); });
-	self.labelRights = ko.computed(function () { return self.getTranslation("labelRights"); });
-	self.labelDesignedBy = ko.computed(function () { return self.getTranslation("labelDesignedBy"); });
+
 	
 	self.init = function () {
 		var $document = $(document);
@@ -101,6 +105,8 @@
 	
 	self.homeLangClickedHandler = function () {
 		self.language(this.getAttribute("data-lang"));
+		if (localStorage)
+			localStorage.language = self.language();
 		self.tab("home");
 		self.setupMainPage();
 	};
@@ -115,6 +121,8 @@
 	
 	self.langClickedHandler = function () {
 		self.language(self.language() === "en" ? "fr" : "en");
+		if (localStorage)
+			localStorage.language = self.language();
 	};
 	// KO observable event handler which updates the url hash whenever the language is changed
 	self.language.subscribe(function (value) {
@@ -183,7 +191,9 @@
 	};
 	
 	return {
-		initialize: self.init
+		initialize: self.init,
+		getTranslation: self.getTranslation,
+		currentYear: self.currentYear
 	};
 })();
 
